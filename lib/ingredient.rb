7369ceb1eea34832ddc37b0154b99c483547d161
aromsa <annacbateman@gmail.com>
2020-05-06 19:43:16 -0400

    refactorv1

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -2,31 +2,7 @@ class Ingredient < ActiveRecord::Base
     has_many :recipe_ingredients
     has_many :recipes, through: :recipe_ingredients
 
-    def ingredient_selection
-        input = gets.chomp 
-        included = ingredients.include?(input)
-        if included
-            find_recipe_by_ingredient(input)
-        else 
-            puts "Hmmm, I can't seem to find that ingredient.  Please select an ingredient from the list below."
-            ingredient_greet
-        end
-    end
-    
-    def ingredients
-        Ingredient.all.map do |ingredient|
-            ingredient.name
-        end
-    end
-    
-    def ingredient_greet
-        puts "\nHello there!! Type an ingredient from the list below!\n
-        \n"
-        Ingredient.all.each do |ingredient|
-            name = ingredient.name
-            puts "\u2022 #{name} \u2022
-            \n"
-        end
-        ingredient_selection
+    def self.names
+        Ingredient.name
     end
 end
--- lib/recipe.rb
+++ lib/recipe.rb
@@ -4,48 +4,13 @@ class Recipe < ActiveRecord::Base
 
     def self.pescatarian 
         Recipe.where(:pescatarian => true)
-        # recipe_names = []        
-        # Recipe.all.select do |recipes|
-        #     if recipes.pescatarian? == true
-        #     recipe_names << recipes.name
-        #     end
-        # end
-        # puts recipe_names
     end
     
     def self.vegetarian
         Recipe.where(:vegetarian => true)
-        # recipe_names = []        
-        # Recipe.all.select do |recipes|
-        #     if recipes.vegetarian? == true
-        #     recipe_names << recipes.name
-        #     end
-        # end
-        # puts recipe_names
     end 
     
     def self.gluten_free
-        # Recipe.all.select{|r| r.gluten_free?} #=> SELECT * FROM recipes
-        Recipe.where(:gluten_free => true) #-> SELECT * FROM recipes WHERE gluten_free = true
-
-        # find_by : detect (Array#find)
-        # where : select
+        Recipe.where(:gluten_free => true)
     end 
-    
-    # def self.all
-    #     recipe_names = []        
-    #     Recipe.all.select do |recipes|
-    #         recipe_names << recipes.name
-    #     end
-    #     puts recipe_names
-    # end 
-
-
-    def find_recipe_by_ingredient(i=nil)
-    Ingredient.all.select do |ingredient|
-        if ingredient.name == i
-        puts ingredient.recipes
-        end
-    end
-end 
 end
